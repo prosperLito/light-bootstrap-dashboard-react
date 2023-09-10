359ea810c1926970779c57e83c9c3b567968e9c3
prosperigoni <prosperigoni@gmail.com>
2023-09-10 22:40:56 +0000

    small changes

--- src/views/Icons.js
+++ src/views/Icons.js
@@ -19,27 +19,36 @@ function Icons() {
       <Container fluid>
         <div>
           <Card>
-              <img src={require("assets/img/Giving Information On The Best Goods & Services Worth Investing In 2023(20).png")} className="rounded mx-auto d-block" >
-             </img>
+          <text > 
+       <h2 className="text-center" >
+            ABOUT US</h2>
+           <p> <h4 className="text-center"></h4></p>
+          </text>
              <Card.Body>
-             <img src={require("assets/img/Giving Information On The Best Goods & Services Worth Investing In 2023(26).png")} className="rounded mx-auto d-block" >
-             </img>
+             I MISSED SHOPPING IS AN ONLINE BUYING AND SELLING SITE, WE ALSO PROMOTES, ADVERTISE, MARKET AND SURVEY ANY PRODUCT AND SERVICES.
+             I MISSED SHOPPING HAS BECOME A GAME CHANGER IN THE WORLD OF E-COMMERCE OUR FEATURES ARE NON-EXISTENT WHICH MAKE US STANDOUT FROM OTHER E-COMMERCE SITES IN THE MARKET TODAY,OUR GOALS
+             ARE DEFINTE AS WE ARE THERE TO IMPACT HUMAN LIFE THAN FOR PROFIT MAKING. WE EDUCATE PEOPLE ON PRODUCTS AND SERVICES THAT INTEREST THEM, THERE ARE SO MANY OF OUR SERVICES THAT ARE FREE TO USE
+             TRY US TODAY AND KNOW IF YOU ARE GOING TO MISS US OR NOT <em>I MISSED SHOPPING</em>
               </Card.Body>
-              </Card>
+                        </Card>
 
 </div>
 
 <div>
   <Card>
-    <img src={require("assets/img/Giving Information On The Best Goods & Services Worth Investing In 2023(22).png")}>
-    </img>
-    <Card.Header as={TypeH3} >
- INFORMATIONS IS GREAT KEY OF EVERY BUSINESS I MISSED SHOPPING ARE THERE TO POWER YOU WITH NECESSARY INFORMATION YOU NEED WHICH ARE AS FOLLOWS</Card.Header>
-
-    <Card.Body as="h5">
-    <img src={require("assets/img/Giving Information On The Best Goods & Services Worth Investing In 2023(27).png")} className="rounded mx-auto d-block" >
-             </img>   
- 
+  <text > 
+       <h2 className="text-center" >
+            OUR AIM</h2>
+           <p> <h4 className="text-center"></h4></p>
+          </text>
+    <Card.Body>
+      <ol>
+ <p>WE KEEP YOU ON A SAFER SIDE WHEN PURCHASING FROM US</p>
+ <p>GET ASSISTANCE FROM OUR MARKETERS AND SKILLS EXPERTS FOR ENQUIRY OF ANY PRODUCT AND SERVICES</p>
+ <p>WE ALSO PROMOTE AND MAKE AWARENESS OF SOME PRODUCTS AND SERVICES AND GET YOU PROSPECTS </p>
+ <p>WE ALSO PROMOTE SKILLS AND TALENTS THROUGH OUR ENTERTAINMENT EXPERTS</p>
+ <p>WE HAVE MANY TO OFFER JUST CONTACT US TO KNOW MORE</p>
+ </ol>
     </Card.Body>
   </Card>
 </div>
@@ -48,10 +57,15 @@ function Icons() {
 
 <div>
   <Card >
-    <img src={require("assets/img/Giving Information On The Best Goods & Services Worth Investing In 2023(23).png")}></img>
+  <text > 
+       <h2 className="text-center" >
+            SUMMARY</h2>
+           <p> <h4 className="text-center"></h4></p>
+          </text>
     <Card.Body >
-    <img src={require("assets/img/Giving Information On The Best Goods & Services Worth Investing In 2023(28).png")} className="rounded mx-auto d-block" >
-             </img>
+    MAKE A STEP FROM WHERE YOU ARE INTO ANOTHER LEVEL WITH I MISSED SHOPPING, DONT WASTE TIME, SHOP WITH US
+    AS AN INDIVIDUAL FOR YOU TO HAVE THE BEST QUALITY OF WHAT YOU WANT, PARTNER WITH US AS A COMPANY AND SEE YOUR COMPANY GROWTH WITH OUR ADVERTISING STRATEGIES,
+    I MISSED SHOPPING BRINGING VALUES TO THE MARKET PLACE  
  </Card.Body>
   </Card>
 </div>
